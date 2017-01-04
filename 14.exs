2a1384e4c564981fe9615c350bc2d79d7e1cfb8a
Heikki Haapala <hhaapala@gmail.com>
2017-01-04 14:39:46 +0200

    Add problem 14 solution

---
+++ 14.exs
@@ -0,0 +1,36 @@
+# The following iterative sequence is defined for the set of positive integers:
+#
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
+#
+# Using the rule above and starting with 13, we generate the following
+# sequence:
+#
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+# It can be seen that this sequence (starting at 13 and finishing at 1)
+# contains 10 terms. Although it has not been proved yet (Collatz Problem), it
+# is thought that all starting numbers finish at 1.
+#
+# Which starting number, under one million, produces the longest chain?
+#
+# NOTE: Once the chain starts the terms are allowed to go above one million.
+require Integer
+
+chain_length = fn number ->
+  count = Stream.unfold(number, fn n ->
+    cond do
+      Integer.is_even(n) ->
+        {n, div(n, 2)}
+      Integer.is_odd(n) ->
+        {n, 3 * n + 1}
+    end
+  end)
+    |> Stream.take_while(&(&1 != 1))
+    |> Enum.count
+
+  count + 1
+end
+
+1..999999
+  |> Enum.max_by(&chain_length.(&1))
+  |> IO.puts
